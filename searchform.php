afa4991691cdb49c31ed818d47cea7e37ec3fd41
Andy Noelker <ajnoelker@fitness-plus.net>
2016-07-26 13:15:25 -0500

    Add PSR-2 formatting

--- searchform.php
+++ searchform.php
@@ -6,8 +6,10 @@
  * @since Aplos 1.0.0
  */
 ?>
-    <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-        <label for="s" class="assistive-text"><?php _e( 'Search', 'aplos' ); ?></label>
-        <input type="text" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', 'aplos' ); ?>" />
-        <input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'aplos' ); ?>" />
+
+    <form method="get" id="searchform" action="<?php echo esc_url(home_url('/')); ?>" role="search">
+        <label for="s" class="assistive-text"><?php _e('Search', 'aplos'); ?></label>
+        <input type="text" class="field" name="s" value="<?php echo esc_attr(get_search_query()); ?>" id="s" placeholder="<?php esc_attr_e('Search &hellip;', 'aplos'); ?>" />
+        <input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e('Search', 'aplos'); ?>" />
     </form>
+
