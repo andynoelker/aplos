0d9c5b02b0c89d057c1f157672ca457c145e44c1
Andy Noelker <ajnoelker@fitness-plus.net>
2016-08-05 16:07:35 -0500

    Add collapsible hamburger menu (#3)

--- header.php
+++ header.php
@@ -25,16 +25,17 @@
 <body <?php body_class(); ?>>
 
 <div id="page" class="hfeed site">
-     <header id="masthead" class="site-header" role="banner">
-         <hgroup>
-     		<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
-     		<h2 class="site-description"><?php bloginfo('description'); ?></h2>
-		</hgroup>
-         <nav role="navigation" class="site-navigation main-navigation">
-         	<h1 class="assistive-text"><?php _e('Menu', 'aplos'); ?></h1>
-			<div class="assistive-text skip-link"><a href="#content" title="<?php esc_attr_e('Skip to content', '_s'); ?>"><?php _e('Skip to content', 'aplos'); ?></a></div>
-			<?php wp_nav_menu(array( 'theme_location' => 'primary' )); ?>
-         </nav><!-- .site-navigation .main-navigation -->
-     </header><!-- #masthead .site-header -->
+    <header id="masthead" class="site-header" role="banner">
+        <hgroup>
+            <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+            <h2 class="site-description"><?php bloginfo('description'); ?></h2>
+        </hgroup>
+        <nav role="navigation" class="site-navigation main-navigation">
+            <h1 class="assistive-text"><?php _e('Menu', 'aplos'); ?></h1>
+            <div class="assistive-text skip-link"><a href="#content" title="<?php esc_attr_e('Skip to content', '_s'); ?>"><?php _e('Skip to content', 'aplos'); ?></a></div>
+            <button class="menu-toggle">&#9776;</button>
+            <?php wp_nav_menu(array( 'theme_location' => 'primary' )); ?>
+        </nav><!-- .site-navigation .main-navigation -->
+    </header><!-- #masthead .site-header -->
 <div id="main" class="site-main">
 
