115b46c998727d70fa286126c613f69105084931
Andy Noelker <ajnoelker@fitness-plus.net>
2016-08-18 12:08:55 -0500

    Only show comment blocks on pages with comments enabled

--- page.php
+++ page.php
@@ -15,7 +15,9 @@ get_header(); ?>
 
                     <?php get_template_part('content', 'page'); ?>
 
-                    <?php comments_template('', true); ?>
+                    <?php if (comments_open() || '0' != get_comments_number()) {
+                        comments_template('', true);
+                    } ?>
 
                 <?php endwhile; // end of the loop. ?>
 
